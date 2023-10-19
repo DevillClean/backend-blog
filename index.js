e37b621bfe5ba08c798075419037f4c6a9534bd9
DevillClean <sudziro62@gmail.com>
2023-10-19 16:02:56 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const config = require('config')
+const authRouter = require('./routes/auth.routes')
+const postsRouter = require('./routes/posts.routes')
+const app = express()
+const PORT = 8080
+
+app.use(express.json())
+app.use('/api/auth', authRouter)
+app.use('/api/posts', postsRouter)
+
+const start = async () => {
+    try {
+        await mongoose.connect(config.get("dbUrl"), {
+            useNewUrlParser:true,
+            useUnifiedTopology:true,
+        });
+        app.listen(PORT, () => console.log(`Server has been started on port`, PORT))
+    } catch (e) {
+        console.log('Server start error',e)
+    }
+}
+
+start()
